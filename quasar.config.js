1a753a66df7445851750b12cede0eab340a05e27
_tango <shurinskiy@mail.ru>
2023-07-01 19:39:09 +0600

    build project

--- quasar.config.js
+++ quasar.config.js
@@ -65,7 +65,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '',
       // analyze: true,
       // env: {},
       // rawDefine: {}
